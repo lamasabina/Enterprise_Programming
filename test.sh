bcdaf934af42a127ff61bf75ec955874885ac9db
Sabina Ghising <sabinaghising@Sabinas-MacBook-Air.local>
2026-01-26 19:16:05 +0545

    Add enterprise coursework: Parallel vs Sequential sorting algorithms with performance analysis

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "=== Sorting Algorithm Test ==="
 echo ""
-echo "Testing with 20,000 elements:"
+echo "Testing with 10,000 elements:"
 echo "------------------------------"
 make clean
 make
 echo ""
-./bubble_seq 20000
-./merge_seq 20000
-./bubble_par 20000 4
-./merge_par 20000 4
+./bubble_seq 10000
+./merge_seq 10000
+./bubble_par 10000 4
+./merge_par 10000 4
